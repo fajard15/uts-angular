77681d6f5340d4d694b7b4f050ea07109d3562f7
fajard15 <092022090220@student.jgu.ac.id>
2025-05-17 00:51:22 +0700

    Create polyfills.ts

---
+++ src/polyfills.ts
@@ -0,0 +1,15 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Add polyfills needed for IE9, IE10, and IE11 if you support them.
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
